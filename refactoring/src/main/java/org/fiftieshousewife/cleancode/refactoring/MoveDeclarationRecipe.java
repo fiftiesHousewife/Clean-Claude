c3f1d963b37b24bf97cbbade5c374e2eeca6c5c5
Pippa Newbold <pippa.newbold@gmail.com>
2026-04-17 09:41:32 +0100

    fix: MoveDeclarationRecipe (G10, G30, N5)

--- refactoring/src/main/java/org/fiftieshousewife/cleancode/refactoring/MoveDeclarationRecipe.java
+++ refactoring/src/main/java/org/fiftieshousewife/cleancode/refactoring/MoveDeclarationRecipe.java
@@ -34,50 +34,69 @@ public class MoveDeclarationRecipe extends Recipe {
 
     @Override
     public TreeVisitor<?, ExecutionContext> getVisitor() {
-        return new JavaIsoVisitor<>() {
-            @Override
-            public J.Block visitBlock(J.Block block, ExecutionContext ctx) {
-                final J.Block b = super.visitBlock(block, ctx);
-                final List<Statement> statements = b.getStatements();
-                if (statements.size() < minDistance + 1) {
-                    return b;
-                }
-
-                final List<Statement> reordered = new ArrayList<>(statements);
-                boolean changed = false;
-
-                for (int i = 0; i < reordered.size(); i++) {
-                    if (!(reordered.get(i) instanceof J.VariableDeclarations varDecl)) {
-                        continue;
-                    }
-                    if (isStaticOrFinal(varDecl)) {
-                        continue;
-                    }
-
-                    final String varName = varDecl.getVariables().getFirst().getSimpleName();
-                    final int firstUse = findFirstUse(reordered, varName, i + 1);
-
-                    if (firstUse < 0 || (firstUse - i) < minDistance) {
-                        continue;
-                    }
-
-                    final Statement removed = reordered.remove(i);
-                    reordered.add(firstUse - 1, removed);
-                    changed = true;
-                    i--;
-                }
-
-                return changed ? b.withStatements(reordered) : b;
-            }
-        };
+        return new MoveDeclarationVisitor(minDistance);
     }
 
-    private static boolean isStaticOrFinal(J.VariableDeclarations varDecl) {
+    private static final class MoveDeclarationVisitor extends JavaIsoVisitor<ExecutionContext> {
+
+        private final int minDistance;
+
+        private MoveDeclarationVisitor(final int minDistance) {
+            this.minDistance = minDistance;
+        }
+
+        @Override
+        public J.Block visitBlock(final J.Block block, final ExecutionContext ctx) {
+            final J.Block visitedBlock = super.visitBlock(block, ctx);
+            final List<Statement> statements = visitedBlock.getStatements();
+            if (statements.size() < minDistance + 1) {
+                return visitedBlock;
+            }
+            return reorderDeclarations(visitedBlock, statements);
+        }
+
+        private J.Block reorderDeclarations(final J.Block block, final List<Statement> statements) {
+            final List<Statement> reordered = new ArrayList<>(statements);
+            final boolean changed = moveDeclarationsInPlace(reordered);
+            return changed ? block.withStatements(reordered) : block;
+        }
+
+        private boolean moveDeclarationsInPlace(final List<Statement> reordered) {
+            boolean changed = false;
+            for (int i = 0; i < reordered.size(); i++) {
+                final int newIndex = tryMoveDeclarationAt(reordered, i);
+                if (newIndex != i) {
+                    changed = true;
+                    i = newIndex;
+                }
+            }
+            return changed;
+        }
+
+        private int tryMoveDeclarationAt(final List<Statement> reordered, final int index) {
+            if (!(reordered.get(index) instanceof J.VariableDeclarations varDecl)) {
+                return index;
+            }
+            if (isStaticOrFinal(varDecl)) {
+                return index;
+            }
+            final String varName = varDecl.getVariables().getFirst().getSimpleName();
+            final int firstUse = findFirstUse(reordered, varName, index + 1);
+            if (firstUse < 0 || (firstUse - index) < minDistance) {
+                return index;
+            }
+            final Statement removed = reordered.remove(index);
+            reordered.add(firstUse - 1, removed);
+            return index - 1;
+        }
+    }
+
+    private static boolean isStaticOrFinal(final J.VariableDeclarations varDecl) {
         return varDecl.getModifiers().stream().anyMatch(m ->
                 m.getType() == J.Modifier.Type.Static || m.getType() == J.Modifier.Type.Final);
     }
 
-    private static int findFirstUse(List<Statement> statements, String varName, int startIndex) {
+    private static int findFirstUse(final List<Statement> statements, final String varName, final int startIndex) {
         for (int i = startIndex; i < statements.size(); i++) {
             if (statements.get(i).toString().contains(varName)) {
                 return i;
